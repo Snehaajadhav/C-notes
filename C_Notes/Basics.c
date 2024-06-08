3401f70e69aa956d1bbcbe3f5716eefddcf2fd4d
Snehaajadhav <sjo418955@gmail.com>
2024-06-08 12:08:27 +0530

    Add files via upload

---
+++ C_Notes/Basics.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// void main()
+// {
+//     int a,b,c;
+//     printf("Enter a number for a = ");
+//     scanf("%d",&a);
+//     printf("Enter a number for b = ");
+//     scanf("%d",&b);
+//     c = a+b;
+//     printf("addition of numbers = %d",c);
+// }
+
+
+// AREA OF CIRCLE
+
+#include <stdio.h>
+void main()
+{
+    float r,area;
+    printf("Enter the radius = ");
+    scanf("%f",&r);
+    area = 3.14 * r * r;
+    printf("Area = %f",area);
+}
